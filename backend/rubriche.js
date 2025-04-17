e67c9dd3fbfac39fda2049fa8b2794b71deb1370
prcant83 <101451378+prcant83@users.noreply.github.com>
2025-04-17 23:11:43 +0200

    Update rubriche.js

--- backend/rubriche.js
+++ backend/rubriche.js
@@ -1,1 +1,20 @@
-// TODO: implement rubriche logic
+// backend/rubriche.js
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/soci.sqlite');
+
+function getRubriche(callback) {
+  db.all("SELECT DISTINCT rubrica FROM soci", [], (err, rows) => {
+    if (err) return callback(err);
+    const rubriche = rows.map(r => r.rubrica);
+    callback(null, rubriche);
+  });
+}
+
+function getContattiRubrica(nome, callback) {
+  db.all("SELECT * FROM soci WHERE rubrica = ?", [nome], (err, rows) => {
+    if (err) return callback(err);
+    callback(null, rows);
+  });
+}
+
+module.exports = { getRubriche, getContattiRubrica };
