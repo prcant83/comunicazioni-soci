c6955720e01e19242b056d5f47b9312132e0f084
prcant83 <101451378+prcant83@users.noreply.github.com>
2025-04-17 23:07:04 +0200

    Update whatsapp.js

--- backend/whatsapp.js
+++ backend/whatsapp.js
@@ -1,1 +1,41 @@
-// TODO: implement WhatsApp logic
+// backend/whatsapp.js
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+const fs = require('fs');
+
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let client;
+
+async function startWhatsApp() {
+  let sessionData;
+  if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require('../whatsapp-session.json');
+  }
+
+  client = new Client({
+    session: sessionData,
+    puppeteer: {
+      headless: true,
+      executablePath: '/usr/bin/chromium-browser',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+  });
+
+  client.on('qr', (qr) => {
+    console.log('🔐 Scansiona il QR Code per accedere a WhatsApp');
+    qrcode.generate(qr, { small: true });
+  });
+
+  client.on('authenticated', (session) => {
+    console.log('🔑 Autenticazione WhatsApp avvenuta con successo');
+    fs.writeFileSync(SESSION_FILE_PATH, JSON.stringify(session));
+  });
+
+  client.on('ready', () => {
+    console.log('✅ WhatsApp client connesso e pronto');
+  });
+
+  await client.initialize();
+}
+
+module.exports = { startWhatsApp };
