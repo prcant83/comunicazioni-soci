1fa7a9f96548bd7664bd12ee63747dee289b52a5
prcant83 <101451378+prcant83@users.noreply.github.com>
2025-04-20 21:06:34 +0200

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -1,15 +1,19 @@
+// utils/log.js
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database/soci.sqlite');
 
-function salvaLogInvio(tipo, destinatario, messaggio, rubrica = null) {
-  const data = new Date().toISOString();
-  db.run(
-    `INSERT INTO log_invio (tipo, destinatario, messaggio, data, rubrica) VALUES (?, ?, ?, ?, ?)`,
-    [tipo, destinatario, messaggio, data, rubrica],
-    (err) => {
-      if (err) console.error('❌ Errore salvataggio log:', err);
+function salvaLogInvio(tipo, destinatario, messaggio, rubrica = '', allegato = '') {
+  const sql = `
+    INSERT INTO log_invio (data, tipo, destinatario, rubrica, messaggio, allegato)
+    VALUES (datetime('now', 'localtime'), ?, ?, ?, ?, ?)
+  `;
+  db.run(sql, [tipo, destinatario, rubrica, messaggio, allegato], function(err) {
+    if (err) {
+      console.error('❌ Errore salvataggio log:', err.message);
+    } else {
+      console.log(`📚 Log salvato: ${tipo} -> ${destinatario}`);
     }
-  );
+  });
 }
 
 module.exports = { salvaLogInvio };
